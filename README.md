0f6a3cf9f4cb1176e9df1ce033c4d6dc5d60a34c
Michael Peltzer <michaelpeltzer@Michaels-MacBook-Air.local>
2017-10-04 18:14:37 -0700

    final touches on javascript and readme

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # train-scheduler
+
+Synopsis: This web application will allow user to create a schedule for train arrivals - it allows the user to input a start time and also duration of time of for the train ride and will then use the current time to calculate a response with regards to when the next train arrives.  
+
+Code Example:  This code shows an the calculations that will generate the time schedule:
+
+var diffCalc = moment().diff(moment.unix(firstTrain), "minutes");
+    
+    var minAway = moment().diff(moment.unix(firstTrain), "minutes") % frequencyFormat;
+    
+    var minCalc = moment(frequencyFormat - minAway, "mm").format("mm");
+    
+    var nextTrain = moment().add(minCalc, "minutes").format("hh:mm a");
+
+    console.log(frequencyFormat); 
+
+Motivation:  To create a functional tool that can be applied to real life situations with regards to scheduling (this does not need to just be applied to trains)
+
+Firebase Reference link: https://console.firebase.google.com/project/train-scheduler-52f6a/database/data
+
+License: Sorry Clay, meant to ask you about this for last weeks HW - not sure what this is 
+referencing
+
+Authors: Michael Peltzer
--- assets/logic.js
+++ assets/logic.js
@@ -1,6 +1,5 @@
 
-
-// Initialize Firebase
+// Link Firebase (generated from the website)
 var config = {
     apiKey: "AIzaSyBshxD5b7peyG2XAssccSpX7GIY-jKaucQ",
     authDomain: "train-scheduler-52f6a.firebaseapp.com",
@@ -10,31 +9,32 @@ var config = {
     messagingSenderId: "1037083854583"
 };
 
+// initializes firebase
 firebase.initializeApp(config);
 
 var database = firebase.database();
 
 
 // on click function for each div id 
-$("#submit-button").on("click", function( ) {
+$("#submit-button").on("click", function() {
     event.preventDefault();
 
     // create variables for each category of the train schedule
     var trainName = $("#train-name").val().trim();
     var destination = $("#destination").val().trim();
-    var firstTrain = $("#first-train").val().trim();
-    var frequency = $("#frequency").val().trim();
+    var firstTrain = moment($("#first-train").val().trim(), "h:mm").format("X");;
+    var frequency = moment($("#frequency").val().trim(), "mm").format("X");
 
     // variable to contain all variables above
     var newTrain = {
-      name: trainName,
-      dest: destination,
-      first: firstTrain,
-      freq: frequency 
+        name: trainName,
+        dest: destination,
+        first: firstTrain,
+        freq: frequency
     };
-    
+
     // pushes newTrain info to firebase
-    database.ref().push(newTrain); 
+    database.ref().push(newTrain);
     console.log(newTrain);
 
     // clears the input field on 'submit' click
@@ -43,22 +43,47 @@ $("#submit-button").on("click", function( ) {
     $("#first-train").val("");
     $("#frequency").val("");
 
-    var currentTime = moment().format("HH:mm");
 
-    console.log(currentTime);
-
-
-
-
+  });
+  
+  // takes a 'snapshot' of the specific child within the newTrain variable and adds to firebase
+  database.ref().on("child_added", function(childSnapshot) {
 
 
+    var trainName = childSnapshot.val().name;
+    var destination = childSnapshot.val().dest;
+    var firstTrain = childSnapshot.val().first;
+    var frequency = childSnapshot.val().freq;
 
+    
+    var firstTrainFormat = moment.unix(firstTrain).format("hh:mm a");
+    var frequencyFormat = moment.unix(frequency).format("mm");
 
+    // time calculations / calculates when the next train arrives based on current time
+    // vs frequency of train routes 
+    
+    var diffCalc = moment().diff(moment.unix(firstTrain), "minutes");
+    
+    var minAway = moment().diff(moment.unix(firstTrain), "minutes") % frequencyFormat;
+    
+    var minCalc = moment(frequencyFormat - minAway, "mm").format("mm");
+    
+    var nextTrain = moment().add(minCalc, "minutes").format("hh:mm a");
 
-    // populates webpage with variable info
-    $("#mikesTable").prepend("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" + firstTrain + "</td><td>" + 
-      frequency + "</td></tr>");
-});
+    console.log(frequencyFormat);
 
+    // appends user input to the table on the webpage
+    $("#mikesTable")
+        .append($("<tr>")
+        .append($("<td>").append(trainName))
+        .append($("<td>").append(destination))
+        .append($("<td>").append(frequencyFormat))
+        .append($("<td>").append(nextTrain))
+        .append($("<td>").append(minCalc))
+
+      );  
+      console.log(trainName);  
+
+    });
 
 
